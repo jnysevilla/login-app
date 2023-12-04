10096b1a0125594ce0720baa96f39ad850047d9e
Jenny Sevilla <jnysevilla@gmail.com>
2023-12-04 22:16:08 +0000

    modify: updated `login.jsx` files and `index.css`

--- src/App.js
+++ src/App.js
@@ -1,22 +1,49 @@
-import React from 'react';
-
-// import { 
-//   BrowserRouter as Router,
-//   Route,
-//   Routes
-// } from 'react-router-dom';
-
+// App.js
+import React, { useState } from 'react';
 import './App.css';
-import Button from './components/Button/Button.js';
+import { Login, UnsuccessfulLogin, SuccessfulLogin } from "../src/components/login";
 
 function App() {
+  const [loginStatus, setLoginStatus] = useState(false);
+  const [showUnsuccessfulLogin, setShowUnsuccessfulLogin] = useState(false);
+
+  const handleSuccessfulLogin = () => {
+    setLoginStatus(true);
+    setShowUnsuccessfulLogin(false); // Reset unsuccessful login status
+  };
+
+  const handleUnsuccessfulLogin = () => {
+    setShowUnsuccessfulLogin(true);
+  };
+
+  const handleTryAgain = () => {
+    setShowUnsuccessfulLogin(false); // Reset unsuccessful login status
+  };
+
+  const handleLogout = () => {
+    setLoginStatus(false);
+  };
+
+  const renderContent = () => {
+    if (showUnsuccessfulLogin) {
+      return <UnsuccessfulLogin onTryAgain={handleTryAgain} />;
+    }
+
+    if (loginStatus) {
+      return <SuccessfulLogin onLogout={handleLogout} />;
+    } else {
+      return (
+        <Login
+          onSuccessfulLogin={handleSuccessfulLogin}
+          onUnsuccessfulLogin={handleUnsuccessfulLogin}
+        />
+      );
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Welcome to my Page!</h1>
-
-        <Button />
-      </header>
+      {renderContent()}
     </div>
   );
 }
